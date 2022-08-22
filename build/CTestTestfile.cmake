407fe226fbc7d239fa1b56a9e78394390d9c1083
Sergey Zabolotny <cepxuozab@gmail.com>
2022-08-22 07:26:17 +0300

    create repo
    example Gtest with conan & cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Dell/Desktop/Testing
+# Build directory: C:/Users/Dell/Desktop/Testing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("testing")
---
+++ build/testing/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Dell/Desktop/Testing/testing
+# Build directory: C:/Users/Dell/Desktop/Testing/build/testing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(fact_gtests "fact_test")
+set_tests_properties(fact_gtests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Dell/Desktop/Testing/testing/CMakeLists.txt;25;add_test;C:/Users/Dell/Desktop/Testing/testing/CMakeLists.txt;0;")
