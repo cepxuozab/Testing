b69e8421958f1cac044b5b9947e76396d1965270
Sergey Zabolotny <cepxuozab@gmail.com>
2022-08-22 07:27:12 +0300

    delete build dir

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dell/Desktop/Testing
-# Build directory: C:/Users/Dell/Desktop/Testing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("testing")
